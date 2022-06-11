2358d85a3e862a8a3ae378c7f47f838b7ed7e2a5
qyjy4737 <qyjy4737@163.com>
2022-06-11 10:58:41 +0800

    修改原因：提交测试 修改人：qyjy4737

---
+++ src/main/java/cn/glonk/api/june/Test0611.java
@@ -0,0 +1,17 @@
+package cn.glonk.api.june;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author： Yang
+ * @date： 2022/6/11
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class Test0611 {
+
+    public static void main(String[] args) {
+        System.out.println("i am coming");
+    }
+}
